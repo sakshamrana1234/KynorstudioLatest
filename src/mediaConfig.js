76975385b0b15e22ba97353c56c361a09f5ea149
Saksham Rana <153150021+sakshamrana1234@users.noreply.github.com>
2026-08-31 00:44:53 +0530

    Update mediaConfig.js

--- src/mediaConfig.js
+++ src/mediaConfig.js
@@ -1,17 +1,17 @@
 export const mediaConfig = {
   'penrose-claymation': {
     embedUrl: 'https://www.youtube-nocookie.com/embed/0Xe9i6OjrNM?rel=0&playsinline=1',
-    poster: '',
+    poster: 'https://i.ytimg.com/vi/0Xe9i6OjrNM/maxresdefault.jpg',
     alt: 'Penrose Skin claymation advertisement'
   },
   'notion-second-brain': {
     embedUrl: 'https://www.youtube-nocookie.com/embed/7xboh7JUChY?rel=0&playsinline=1',
-    poster: '',
+    poster: 'https://i.ytimg.com/vi/7xboh7JUChY/maxresdefault.jpg',
     alt: 'Notion second-brain vertical concept advertisement'
   },
   'memzero-launch': {
     embedUrl: 'https://www.youtube-nocookie.com/embed/nKbitVwDCfk?rel=0&playsinline=1',
-    poster: '',
+    poster: 'https://i.ytimg.com/vi/nKbitVwDCfk/maxresdefault.jpg',
     alt: 'MemZero Gateway AI memory product-launch film'
   }
 }
